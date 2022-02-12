3e8f8ca1d3dee9b050fcf183c8eeaa2a8c5dbead
Benoît Amiaux <benoit.amiaux@gmail.com>
2022-02-12 12:33:58 +0100

    tado: Add caching

--- tado_away.py
+++ tado_away.py
@@ -16,6 +16,16 @@ logging.basicConfig(
 )
 log = logging.getLogger("rich")
 
+
+class Context:
+    def __init__(self, session: tado.Tado):
+        self.session = session
+        self.zones: dict[int, bool] = {}
+        self.names: dict[int, str] = {}
+        self.home: bool | None = None
+        self.time: float = 0
+
+
 # login creates a tado session from input username/password
 def login(args) -> tado.Tado:
     log.info("login...")
@@ -24,48 +34,118 @@ def login(args) -> tado.Tado:
     return session
 
 
-# is_zone_open returns whether input zone have opened windows
-def is_zone_open(session: tado.Tado, zone: dict) -> bool:
-    zid = zone["id"]
-    data = session.getOpenWindowDetected(zid)
+def read_is_open(zstate: dict) -> bool:
     try:
-        return data["openWindowDetected"]
+        return zstate["openWindow"] is not None
     except:
         return False
 
 
+def read_previous_is_open(ctx: Context, zid: int) -> bool | None:
+    try:
+        return ctx.zones[zid]
+    except:
+        return None
+
+
+def set_open_window(ctx: Context, zid: int, is_open: bool):
+    if is_open:
+        ctx.session.setOpenWindow(zid)
+    else:
+        ctx.session.resetOpenWindow(zid)
+
+
+def cache_zone_names(ctx: Context):
+    data = ctx.session.getZones()
+    if not data:
+        return None
+
+    ctx.names = {}
+    for zone in data:
+        ctx.names[zone["id"]] = zone["name"]
+
+
+def read_zone_name(ctx: Context, zid: int) -> str:
+    try:
+        return ctx.names[zid]
+    except:
+        return "unknown"
+
+
 # check_open_windows disables any zone
 # where opened windows are detected
-def check_open_windows(session: tado.Tado):
-    for zone in session.getZones():
-        name = zone["name"]
-        is_open = is_zone_open(session, zone)
+def check_open_windows(ctx: Context):
+    # read zone states
+    states = ctx.session.getZoneStates()
+    if not states:
+        return
+
+    for zid, zstate in states["zoneStates"].items():
+        # foreach zone check previous & current is_open state
+        zid = int(zid)
+        is_open = read_is_open(zstate)
+        previous_is_open = read_previous_is_open(ctx, zid)
+        if previous_is_open == is_open:
+            continue
+
+        # if those are different, update them
+        set_open_window(ctx, zid, is_open)
+        # update context with latest known state
+        ctx.zones[zid] = is_open
+        name = read_zone_name(ctx, zid)
         zstate = "stopped" if is_open else "running"
         log.info(f"zone {name}: {zstate}")
-        session.setOpenWindow(is_open)
+
+
+def is_any_device_home(ctx: Context) -> bool:
+    # do not set away mode if no devices are found
+    devices = ctx.session.getMobileDevices()
+    if devices is None:
+        return True
+
+    for device in devices:
+        # rich.inspect(device, docs=False)
+        if not device["settings"]["geoTrackingEnabled"]:
+            continue
+        if device["location"]["atHome"]:
+            return True
+    return False
 
 
 # check_far_from_hone checks whether any device
 # is home else stops cooling until one returns
-def check_far_from_home(session: tado.Tado):
-    any_home = False
-    for device in session.getMobileDevices():
-        # rich.inspect(device, docs=False)
-        if not device["settings"]["geoTrackingEnabled"]:
-            continue
-        any_home = any_home or device["location"]["atHome"]
+def check_far_from_home(ctx: Context):
+    any_home = is_any_device_home(ctx)
+    prev_home = ctx.home
+    if prev_home == any_home:
+        return
 
     # set home/away state accordingly
-    state = "home" if any_home else "away"
-    state_fn = session.setHome if any_home else session.setAway
-    log.info(f"devices: {state}")
+    state_fn = ctx.session.setHome if any_home else ctx.session.setAway
     state_fn()
+    ctx.home = any_home
+    state = "home" if any_home else "away"
+    log.info(f"state: {state}")
 
 
-def main(args):
-    session = login(args)
-    check_open_windows(session)
-    check_far_from_home(session)
+def refresh_context(ctx: Context, args):
+    # rich.inspect(ctx)
+    now = time.time()
+    duration = int(now - ctx.time)
+    if duration < args.max_cache_duration:
+        return
+
+    log.info("caching...")
+    ctx.home = None
+    ctx.zones = {}
+    cache_zone_names(ctx)
+    ctx.time = now
+
+
+def main(ctx: Context, args):
+    refresh_context(ctx, args)
+    check_open_windows(ctx)
+    check_far_from_home(ctx)
     return 0
 
 
@@ -77,21 +157,32 @@ def parse_args():
     parser.add_argument(
         "--password", type=str, default=getpass.getpass(), help="tado password"
     )
-    parser.add_argument("--period", type=int, default=60, help="period in seconds")
+    parser.add_argument("--period", type=int, default=10, help="period in seconds")
+    parser.add_argument(
+        "--max-cache-duration",
+        type=int,
+        default=60 * 60,
+        help="max cache duration in seconds",
+    )
     return parser.parse_args()
 
 
 def run_once(args):
-    try:
-        ret = main(args)
-    except Exception:
-        console.print_exception()
-        ret = -1
-    return ret
+    session = login(args)
+    ctx = Context(session)
+    while True:
+        try:
+            main(ctx, args)
+        except Exception:
+            console.print_exception()
+        time.sleep(args.period)
 
 
 if __name__ == "__main__":
     args = parse_args()
     while True:
-        run_once(args)
+        try:
+            run_once(args)
+        except Exception:
+            console.print_exception()
         time.sleep(args.period)
